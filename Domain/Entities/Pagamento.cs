0e0b284587730a391c998ca8be3c3521a001b856
Guilherme Sousa <guilherme.sousa@fpf.org.br>
2025-05-18 16:59:40 -0300

    first commit

---
+++ Domain/Entities/Pagamento.cs
@@ -0,0 +1,39 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Domain.Entities
+{
+    public class Pagamento
+    {
+        public static readonly string[] STATUS = new string[] { "Aguardando", "Pago", "Cancelado" };
+
+        public Pagamento(Guid idPagamento, Guid idPedido, string txId, string statusPagamento, DateTime dataStatusPagamento)
+        {
+            if (!STATUS.Contains(statusPagamento))
+                throw new ArgumentException("O status está incorreto");
+
+            IdPagamento = idPagamento;
+            IdPedido = idPedido;
+            TxId = txId;
+            StatusPagamento = statusPagamento;
+            DataStatusPagamento = dataStatusPagamento;
+        }
+
+        // Construtor sem parâmetros necessário para deserialização
+        private Pagamento() { }
+
+
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public Guid IdPagamento { get; private set; }
+
+        [BsonRepresentation(BsonType.String)]
+        public Guid IdPedido { get; private set; }
+        
+        public string TxId { get; private set; }
+
+        public string StatusPagamento { get; private set; }
+
+        public DateTime DataStatusPagamento { get; private set; }
+    }
+}
---
+++ Tests/Unidade/PagamentoTests.cs
@@ -0,0 +1,49 @@
+﻿using Domain.Entities;
+using FluentAssertions;
+
+namespace Tests.Unidade
+{
+    public class PagamentoTests
+    {
+        [Fact]
+        public void Construtor_Deve_Criar_Pagamento_Valido()
+        {
+            // Arrange
+            var idPagamento = Guid.NewGuid();
+            var idPedido = Guid.NewGuid();
+            var txId = "123456789";
+            var status = "Pago";
+            var dataStatus = DateTime.UtcNow;
+
+            // Act
+            var pagamento = new Pagamento(idPagamento, idPedido, txId, status, dataStatus);
+
+            // Assert
+            pagamento.IdPagamento.Should().Be(idPagamento);
+            pagamento.IdPedido.Should().Be(idPedido);
+            pagamento.TxId.Should().Be(txId);
+            pagamento.StatusPagamento.Should().Be(status);
+            pagamento.DataStatusPagamento.Should().BeCloseTo(dataStatus, TimeSpan.FromSeconds(1));
+        }
+
+        [Theory]
+        [InlineData("Invalido")]
+        [InlineData("")]
+        [InlineData(null)]
+        public void Construtor_Deve_Lancar_Excecao_Para_Status_Invalido(string statusInvalido)
+        {
+            // Arrange
+            var idPagamento = Guid.NewGuid();
+            var idPedido = Guid.NewGuid();
+            var txId = "123456789";
+            var dataStatus = DateTime.UtcNow;
+
+            // Act
+            Action acao = () => new Pagamento(idPagamento, idPedido, txId, statusInvalido, dataStatus);
+
+            // Assert
+            acao.Should().Throw<ArgumentException>()
+                .WithMessage("O status está incorreto");
+        }
+    }
+}
