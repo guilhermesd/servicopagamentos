4e1ba598d7563002d7cf33192a789658668ab005
Guilherme Sousa <guilherme.sousa@fpf.org.br>
2025-05-21 21:38:36 -0300

    feat: Alterado no swagger Serviço de pagamentos

--- Api/Program.cs
+++ Api/Program.cs
@@ -4,6 +4,7 @@ using Domain.Interfaces.Repositories;
 using Domain.Interfaces.ServicosExternos;
 using Infrastructure.Repositories;
 using Infrastructure.ServicosExternos;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +13,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Serviço de Pagamentos", Version = "v1" });
+});
 
 // Injeção de dependências para Repositórios
 builder.Services.AddScoped<IPagamentoContext, PagamentoContext>();
